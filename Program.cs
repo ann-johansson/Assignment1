81649b5ba6295d17d12b010f2b37bd06b7d63808
ann-johansson <ann.johansson.work@gmail.com>
2025-09-02 15:01:16 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Writing someething here...");
+            Console.WriteLine("Writing someething here...Nemo skrev");
         }
     }
 }
