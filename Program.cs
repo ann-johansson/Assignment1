36dd9f3bbea8455a4d0145b12b8214796b24794d
ann-johansson <ann.johansson.work@gmail.com>
2025-09-02 15:03:52 +0200

    a new line added again

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Writing someething here...Nemo skrev");
+            Console.WriteLine("writing to try");
         }
     }
 }
